76489dd4d9be673a0220bb74ab2f30661f2edc3d
Scott Walls <scottwalls2008@gmail.com>
2025-04-16 12:34:57 -0500

    First tool impl

--- go.mod
+++ go.mod
@@ -4,4 +4,15 @@ go 1.24.0
 
 toolchain go1.24.2
 
-require github.com/ollama/ollama v0.6.5
+require (
+	github.com/invopop/jsonschema v0.13.0
+	github.com/ollama/ollama v0.6.5
+)
+
+require (
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/wk8/go-ordered-map/v2 v2.1.8 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strings"
 
+	"github.com/invopop/jsonschema"
 	"github.com/ollama/ollama/api"
 )
 
@@ -28,31 +29,31 @@ func main() {
 		return scanner.Text(), true
 	}
 
-	agent := NewAgent(client, getUserMessage)
+	tools := []ToolDefinition{ReadFileDefintion}
+	agent := NewAgent(client, getUserMessage, tools)
 	err = agent.Run(context.TODO())
 	if err != nil {
 		fmt.Printf("Error: %s\n", err.Error())
 	}
 }
 
-func NewAgent(client *api.Client, getUserMessage func() (string, bool)) *Agent {
+func NewAgent(client *api.Client, getUserMessage func() (string, bool), tools []ToolDefinition) *Agent {
 	return &Agent{
 		client:         client,
 		getUserMessage: getUserMessage,
+		tools:          tools,
 	}
 }
 
+// Agent and methods
 type Agent struct {
 	client         *api.Client
 	getUserMessage func() (string, bool)
+	tools          []ToolDefinition
 }
 
 func (a *Agent) Run(ctx context.Context) error {
-	messages := []api.Message{}
-	conversation := &api.ChatRequest{
-		Model:    MODEL_NAME,
-		Messages: messages,
-	}
+	conversation := []api.Message{}
 
 	fmt.Println("Chat with Ollama (use 'ctrl-c' to quit)")
 
@@ -67,41 +68,210 @@ func (a *Agent) Run(ctx context.Context) error {
 			Role:    "user",
 			Content: userInput,
 		}
-		messages = append(messages, userMessage)
-		conversation.Messages = messages
+		conversation = append(conversation, userMessage)
 
 		fmt.Print("\u001b[93mOllama\u001b[0m: ")
-		responseTokens := []string{}
-		var responder string
-		handler := func(chat api.ChatResponse) error {
-			message := chat.Message
-			fmt.Print(message.Content)
-			responseTokens = append(responseTokens, message.Content)
-			responder = chat.Message.Role
 
-			return nil
+		response := a.runInference(ctx, conversation)
+		conversation = append(conversation, response)
+
+		for {
+			if response.Role != "tool" {
+				break
+			}
+			response = a.runInference(ctx, conversation)
+			conversation = append(conversation, response)
 		}
 
-		err := a.runInference(ctx, conversation, handler)
-		if err != nil {
-			return err
-		}
-
-		response := strings.Join(responseTokens, " ")
-		responseMsg := api.Message{
-			Content: response,
-			Role: responder,
-		}
-		messages = append(messages, responseMsg)
-		conversation.Messages = messages
 		fmt.Print("\n")
 	}
 
 	return nil
 }
 
-func (a *Agent) runInference(ctx context.Context, conversation *api.ChatRequest, handler api.ChatResponseFunc) error {
-	err := a.client.Chat(ctx, conversation, handler)
+func (a *Agent) runInference(ctx context.Context, conversation []api.Message) api.Message {
+	responseTokens := []string{}
+	var responder string
+	handler := func(chat api.ChatResponse) error {
+		message := chat.Message
+		if len(message.ToolCalls) > 0 {
+			for _, call := range message.ToolCalls {
+				toolResp := a.executeTool(call.Function.Name, call.Function.Arguments)
+				responder = toolResp.Role
+				responseTokens = append(responseTokens, toolResp.Content)
+			}
+		}
+		if len(message.Content) > 0 {
+			fmt.Print(message.Content)
+			responseTokens = append(responseTokens, message.Content)
+			responder = chat.Message.Role
+		}
 
-	return err
+		return nil
+	}
+
+	ollamaTools := []api.Tool{}
+	for _, tool := range a.tools {
+		ollamaTools = append(ollamaTools, api.Tool{
+			Type: "function",
+			Function: api.ToolFunction{
+				Name:        tool.Name,
+				Description: tool.Description,
+				Parameters:  tool.InputSchema,
+			},
+		})
+	}
+	chatRequest := &api.ChatRequest{
+		Model:    MODEL_NAME,
+		Messages: conversation,
+		Tools:    ollamaTools,
+	}
+	err := a.client.Chat(ctx, chatRequest, handler)
+	if err != nil {
+		panic(err)
+	}
+
+	response := removeToolTokens(strings.Join(responseTokens, " "))
+	responseMsg := api.Message{
+		Content: response,
+		Role:    responder,
+	}
+	return responseMsg
+}
+
+func (a *Agent) executeTool(name string, arguments api.ToolCallFunctionArguments) api.Message {
+	found := false
+	var toolDef ToolDefinition
+	for _, tool := range a.tools {
+		if tool.Name == name {
+			found = true
+			toolDef = tool
+		}
+	}
+
+	if !found {
+		return api.Message{
+			Role:    "tool",
+			Content: "tool not found",
+		}
+	}
+
+	fmt.Printf("\u001b[92mtool\u001b[0m: %s(%s)\n", name, arguments)
+	response, err := toolDef.Function(arguments)
+	if err != nil {
+		return api.Message{
+			Role:    "tool",
+			Content: err.Error(),
+		}
+	}
+
+	return api.Message{
+		Role:    "tool",
+		Content: response,
+	}
+}
+
+// ToolTypes
+type ToolDefinition struct {
+	Name        string             `json:"name"`
+	Description string             `json:"description"`
+	InputSchema ToolFunctionParams `json:"input_schema"`
+	Function    func(input map[string]any) (string, error)
+}
+
+type ToolFunctionParams struct {
+	Type       string   `json:"type"`
+	Required   []string `json:"required"`
+	Properties map[string]struct {
+		Type        string   `json:"type"`
+		Description string   `json:"description"`
+		Enum        []string `json:"enum,omitempty"`
+	} `json:"properties"`
+}
+
+// ToolDefinitions
+var ReadFileDefintion = ToolDefinition{
+	Name:        "read_file",
+	Description: "Read the contents of a given relative file path. Use this when you want to see what's inside a file. Do not use this with directory names.",
+	InputSchema: ReadFileInputSchema,
+	Function:    ReadFile,
+}
+
+type ReadFileInput struct {
+	Path string `json:"path" jsonschema_description:"The relative path of a file in the working directory."`
+}
+
+var ReadFileInputSchema = GenerateSchema[ReadFileInput]()
+
+func ReadFile(input map[string]any) (string, error) {
+	path, ok := input["path"].(string)
+	if !ok {
+		panic("Attempted to call ReadFile with invalid input")
+	}
+
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+
+	return string(content), nil
+}
+
+// utils
+
+func validateStringArr(input []any) []string {
+	ret := []string{}
+	for _, item := range input {
+		str, ok := item.(string)
+		if !ok {
+			fmt.Printf("Not a string item encountered")
+			continue
+		}
+		ret = append(ret, str)
+	}
+	return ret
+}
+
+func GenerateSchema[T any]() ToolFunctionParams {
+	reflector := jsonschema.Reflector{
+		AllowAdditionalProperties: false,
+		DoNotReference:            true,
+	}
+	var v T
+
+	schema := reflector.Reflect(v)
+	properties := make(map[string]struct {
+		Type        string   `json:"type"`
+		Description string   `json:"description"`
+		Enum        []string `json:"enum,omitempty"`
+	})
+	for pair := schema.Properties.Oldest(); pair != nil; pair = pair.Next() {
+		enum := validateStringArr(pair.Value.Enum)
+		properties[pair.Key] = struct {
+			Type        string   `json:"type"`
+			Description string   `json:"description"`
+			Enum        []string `json:"enum,omitempty"`
+		}{
+			Type:        pair.Value.Type,
+			Description: pair.Value.Description,
+			Enum:        enum,
+		}
+	}
+
+	return ToolFunctionParams{
+		Type:       "object",
+		Required:   schema.Required,
+		Properties: properties,
+	}
+}
+
+func removeToolTokens(response string) string {
+	// Define tool tokens to remove
+	toolTokens := []string{"<|im_start|>", "<|im_end|>"}
+
+	for _, token := range toolTokens {
+		response = strings.ReplaceAll(response, token, "")
+	}
+
+	return response
 }
